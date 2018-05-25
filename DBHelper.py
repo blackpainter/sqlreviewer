1e38432815ac7a98da2b673a2c783e80ea060e19
blackpainter <zy_2626@163.com>
2018-05-25 11:58:57 +0800

    辅助工具

---
+++ DBHelper.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python2.6
+# -- coding: utf-8 --
+
+import datetime, time
+import re
+import os, sys, socket
+
+class Stack(object):
+	items = []
+
+	def __init__(self):
+		self.items = []
+
+	def is_empty(self):
+		return self.items == []
+
+	def peek(self):
+		return self.items[-1]
+
+	def size(self):
+		return len(self.items)
+
+	def push(self, item):
+		self.items.append(item)
+
+	def pop(self):
+		return self.items.pop()
+
+	def __repr__(self):
+		return str(self.items)
+
+#当前log文件大于100M时生成新的log
+def new_file_when_toobig(filepath, size):
+	fsize = os.popen("ls -l %s | awk '{print $5}'" %  filepath).readline().strip()
+	if fsize.isdigit() and int(fsize) >= size:
+		renamecmd = "mv %s %s_%s.log" % (filepath, filepath[:filepath.rfind('.')], time.strftime('%Y%m%d%H%M%S',time.localtime(time.time())))
+		os.system(renamecmd)
+		os.system("touch %s" % filepath)
+	
+#获取本机mysql实例信息
+def get_mysql_instances_local(conf = ["port", "socket", "basedir", "datadir"]):
+	mysql_instances = {}
+
+	for line in os.popen('ps -aux | grep "mysqld "'):
+		ins = {}
+		ll = line.split(" ")
+		if ll[0] == "mysql" or ll[0] == "root" and len(ll)>11:
+			for l in ll:
+				if "=" in l:
+					l = l.replace("--", "")
+					if l[:l.index("=")] in conf:
+						ins[l[:l.index("=")]] = l[l.index("=")+1:].strip()
+				if "=" not in l and '/' in l and 'mysqld' in l:
+					ins["mysql_ext"] =l
+			if "port" in ins.keys() and (ins["port"] not in mysql_instances.keys()) and ins["port"].isdigit():
+				mysql_instances[ins["port"]] = ins
+			elif len(ins.keys())>0 and len(mysql_instances.keys())==0:
+				mysql_instances["3306"] = ins
+	return mysql_instances
+
+#获得本地IP地址
+def get_local_ipadd():
+	ip = ""
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.connect(('8.8.8.8', 80))
+		ip = s.getsockname()[0]
+	finally:
+		s.close()
+	return ip
+
+#检查字符串是否为合法ip地址
+def isIpadd(ipStr):
+	compile_ip=re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
+	if compile_ip.match(ipStr):
+		return True
+	else:
+		return False
+
+#检查本机备份盘剩余空间
+def get_backupdisk_free():
+	largest_disk = 0
+	largest_disk_free = 0
+	backup_disk_free = 0
+
+	for line in os.popen("df"):
+		ll = line.split(" ")
+		if len(ll)>=5 and ll[-1][0] == '/':
+			lastc = ll[-1]
+			if len(lastc)>=6 and lastc[len(lastc)-6:] == "backup" and ll[-2][:len(ll[-2])-1].isdigit():
+				backup_disk_free = int(ll[-2][:len(ll[-2])-1])
+	
+			if ll[-5].isdigit() and int(ll[-5])>largest_disk and ll[-2][:len(ll[-2])-1].isdigit():
+				largest_disk = int(ll[-5])
+				largest_disk_free = int(ll[-2][:len(ll[-2])-1])
+
+	if backup_disk_free <= 0:
+		backup_disk_free = largest_disk_free
+
+	return backup_disk_free
+
---
+++ dbase.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pymysql as mdb
+import re, os, sys
+
+conn_dict = {}
+
+MDB_IP = '172.28.40.202'
+MDB_PORT = 3306
+MDB_USER = 'root'
+MDB_PWD = '123456'
+MDB_DB = 'sqlreviewer'
+
+def getDateString(timer,format="%Y-%m-%d"):
+	return timer.strftime(format)
+
+#通过sql语句得到结果集
+def fetchall(sql, ip = None, port = None, user = None, pwd = None, db = None):
+	MyDB = DBSource(ip, port, user, pwd, db)
+	return MyDB.get_result(sql)
+
+def fetchone(sql, ip = None, port = None, user = None, pwd = None, db = None):
+	MyDB = DBSource(ip, port, user, pwd, db)
+	rowset = MyDB.get_result(sql)
+	for row in rowset:
+		return row
+
+#判断结果集是否为None
+def isfull(rowset):
+	if not rowset[0]:
+		return False
+	else:
+		return True
+
+#将结果集转化成想要的字符串
+def tostring(rowset,name,type=1):
+	if not rowset[0] :
+		return "None"
+	index = rowset.index(name) 
+	string = ""
+	if type == 1:
+		for row in rowset:
+			string += "'%s',"%(str(row[index]).replace("\\","\\\\").replace("'","\\'"))
+	else :
+		for row in rowset:
+			string += "%d,"%(int(row[index]))
+	string = string.rstrip(",")
+	return string 
+
+#保存
+def save(sql, ip = None, port = None, user = None, pwd = None, db = None):
+	MyDB = DBSource(ip, port, user, pwd, db)
+	MyDB.save(sql)
+
+
+class DBSource:
+
+	_Host = MDB_IP
+	_DB = MDB_DB
+	_Port = MDB_PORT
+	_User = MDB_USER
+	_Passwd = MDB_PWD
+
+	_conn = None
+	_cursor = None
+
+	def __init__(self,ip, port, user, pwd, db):
+		if ip is not None:
+			self._Host = ip
+		if port is not None:
+			self._Port = port
+		if user is not None:
+			self._User = user
+		if pwd is not None:
+			self._Passwd = pwd
+		if db is not None:
+			self._DB = db
+
+		self.get_conn(self._Host, self._DB, self._User, self._Passwd, self._Port)
+		self.get_cursor()
+
+	def get_conn(self, host=MDB_IP, db=MDB_DB, user=MDB_USER, passwd=MDB_PWD, port=MDB_PORT):
+		try:
+			key = host + "_" + db + "_" + str(port)
+			#print key
+			if key in conn_dict and conn_dict[key] != None :
+				self._conn = conn_dict[key]
+			else:
+				self._conn = mdb.Connect(host=host, db=db, user=user, passwd=passwd, port=port, charset="utf8")
+				conn_dict[key] = self._conn
+		except Exception as e:
+			raise RuntimeError("Error in connection " + str(e))
+
+	def get_cursor(self):
+		try :
+			self._cursor=self._conn.cursor()
+			self._cursor.execute("set autocommit = 1")
+		except Exception as e:
+			raise RuntimeError("Error in cursor")
+	
+	#关闭连接	
+	def close(self):
+		self._conn.close()
+		self._conn = None
+		self._cursor = None
+
+	#连接数据库
+	def get_connect(self):
+		#print self._Host,self._DB,self._User,self._Passwd,self._Port
+		self.get_conn(self._Host, self._DB, self._User, self._Passwd, self._Port)
+		self.get_cursor()
+
+	#保存
+	def save(self, sql):
+		try:
+			self.get_connect()
+			self._cursor.execute(sql)
+		except Exception as ex:
+			conn_dict[self._Host+"_"+self._DB+"_"+str(self._Port)] = None
+			self.get_connect()
+			self._cursor.execute(sql)
+
+	#query
+	def get_result(self, sql):
+		try:
+			self.get_connect()		
+			self._cursor.execute(sql)
+		except Exception as ex:
+			conn_dict[self._Host+"_"+self._DB+"_"+str(self._Port)] = None
+			self.get_connect()
+			self._cursor.execute(sql)
+		rowset = self._cursor.fetchall()
+		if rowset and len(rowset)>0 :
+			record = RecordSet([list(x) for x in rowset],[x[0].lower( ) for x in self._cursor.description])
+		else:
+			record = [None,]
+		return record
+
+
+#将结果集转化成自定义的recordset的结果	
+class RecordSet:
+	def __init__(self, tableData, columnNames):
+		self.data = tableData
+		self.columns = columnNames
+		self.columnMap = {}
+
+		for name,n in zip(columnNames, range(0,10000)):
+			self.columnMap[name] = n
+
+	def __getitem__(self, n):
+		return Record(self.data[n], self.columnMap)
+
+	def __setitem__(self, n, value):
+		self.data[n] = value
+
+	def __delitem__(self, n):
+		del self.data[n]
+
+	def __len__(self):
+		return len(self.data)
+
+	def __str__(self):
+		return '%s: %s' % (self.__class__,self.columns)
+
+	def index(self,name):
+		i = 0
+		for colnum in self.columns:
+			if colnum == name:
+				return i
+			i += 1
+		return i
+
+	def __nonzero__(self):
+		if len(self.data) == 0 :
+			return False
+		else:
+			return True
+
+	def getcolumn(self):
+		return self.columns
+
+	def getdata(self):
+		return self.data
+
+class Record:
+	def __init__(self, rowData, columnMap):
+		self.__dict__['_data_'] = rowData
+		self.__dict__['_map_'] = columnMap
+
+	def __getattr__(self, name):
+		return self._data_[self._map_[name]]
+
+	def __setattr__(self, name, value):
+		try:
+			n = self._map_[name]
+		except KeyError:
+			self.__dict__[name] = value
+		else:
+			self._data_[n] = value
+
+	def __getitem__(self, n):
+		return self._data_[n]
+
+	def __setitem__(self, n, value):
+		self._data_[n] = value
+
+	def __getslice__(self, i, j):
+		return self._data_[i:j]
+
+	def __setslice__(self, i, j, slice):
+		self._data_[i:j] = slice
+
+	def __len__(self):
+		return len(self._data_)
+
+	def __str__(self):
+		return '%s: %s' % (self.__class__,repr(self._data_))
+
+	def __nonzero__(self):
+		if len(self._data_) == 0:
+			return False
+		else:
+			return True
+
+	def __iter__(self):
+		return iter(self._data_)
+
+	def getdata(self):
+		return self._data_
+
